5f884d870207addf92c1765d34fd1f702e76cf37
Vitor Ramos <gramosvitor@gmail.com>
2024-05-27 11:06:39 -0300

    Created 'List registered user' operation in main menu

--- RegystrationProgram/src/application/TestProgram.java
+++ RegystrationProgram/src/application/TestProgram.java
@@ -1,52 +1,38 @@
 package application;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TestProgram {
 
 	public static void main(String[] args) {
 
-		@SuppressWarnings("unused")
-		boolean loopControl = true;
-		Scanner sc = new Scanner(System.in);
-
-		do {
-			System.out.println("==== Select an Option ====");
-			System.out.println("1 - Register new User");
-			System.out.println("2 - List registered users");
-			System.out.println("3 - Register new question on the form");
-			System.out.println("4 - Delete question from the form");
-			System.out.println("5 - Search User by name or e-mail");
-			System.out.println("6 - Exit");
-			System.out.print("Enter option number: ");
-			int optionNumber = sc.nextInt();
-
-			switch (optionNumber) {
-				case 1:
-					System.out.println("caso 1");
-					break;
-				case 2:
-					System.out.println("caso 2");
-					break;
-				case 3:
-					System.out.println("caso 3");
-					break;
-				case 4:
-					System.out.println("caso 4");
-					break;
-				case 5:
-					System.out.println("caso 5");
-					break;
-				case 6:
-					loopControl = false;
-					break;
-				default:
-					System.out.println("Enter a number between 1 and 6");
+		String dbPath = "D:\\temp\\SistemaDeCadastros\\database";
+		File path = new File(dbPath); File[] files = path.listFiles();
+		int i = 1;
+		for (File file : files) {
+			try {
+				System.out.printf(i + " - " + nameWritter(file) + "%n");
+				i ++;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-		} while (loopControl != false);
+		}
 		
-		sc.close();
+		
+	}
+
+	public static String nameWritter(File file) throws FileNotFoundException, IOException {
+		
+		try (BufferedReader bf = new BufferedReader(new FileReader(file.getPath()))){
+			
+			String name = bf.readLine();
+			return name;
+		}
 	}
 
 }
---
+++ RegystrationProgram/src/services/ListUsers.java
@@ -0,0 +1,29 @@
+package services;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ListUsers {
+
+	public static void listUsers(String dbPath) throws FileNotFoundException, IOException {
+		File path = new File(dbPath);
+		File[] files = path.listFiles();
+		int i = 1;
+		for (File file : files) {
+			System.out.printf(i + " - " + nameWritter(file) + "%n");
+			i++;
+		}
+	}
+
+	public static String nameWritter(File file) throws FileNotFoundException, IOException {
+
+		try (BufferedReader bf = new BufferedReader(new FileReader(file.getPath()))) {
+
+			String name = bf.readLine();
+			return name;
+		}
+	}
+}
